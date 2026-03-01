c921a45e854226694d11e33858ea6190720a9fd5
Raghav893 <raghavarora2309@gmail.com>
2026-03-01 18:56:42 +0530

    initial commit

---
+++ src/test/java/com/raghav/xclone/XcloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raghav.xclone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XcloneApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
