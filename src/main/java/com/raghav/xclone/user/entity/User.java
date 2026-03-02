d04895ea6fe35821bc4c75bcfebcf7c47293870c
Raghav893 <raghavarora2309@gmail.com>
2026-03-02 21:09:01 +0530

    fixes

--- src/main/java/com/raghav/xclone/user/entity/User.java
+++ src/main/java/com/raghav/xclone/user/entity/User.java
@@ -7,6 +7,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
+import java.util.UUID;
 
 @Entity
 @AllArgsConstructor
@@ -14,8 +15,8 @@ import java.time.LocalDateTime;
 @Data
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
 
     @Column(nullable = false,unique = true)
 
