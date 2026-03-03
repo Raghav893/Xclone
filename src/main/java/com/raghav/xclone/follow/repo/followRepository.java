d80c60f1b65d7ebce15a11fcc879a0b8eac021cd
Raghav893 <raghavarora2309@gmail.com>
2026-03-03 19:07:51 +0530

    follower repo

--- src/main/java/com/raghav/xclone/follow/repo/followRepository.java
+++ src/main/java/com/raghav/xclone/follow/repo/followRepository.java
@@ -2,6 +2,9 @@ package com.raghav.xclone.follow.repo;
 
 import com.raghav.xclone.follow.entity.follow;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.RestController;
 
+@Repository
 public interface followRepository extends JpaRepository<follow, Long> {
 }
--- src/main/java/com/raghav/xclone/user/repo/UserRepository.java
+++ src/main/java/com/raghav/xclone/user/repo/UserRepository.java
@@ -3,9 +3,12 @@ package com.raghav.xclone.user.repo;
 import com.raghav.xclone.user.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.awt.print.Pageable;
 
+
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
 
