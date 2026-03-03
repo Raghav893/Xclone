69adbf9f0e3873094d49242e731930bc70dfe31f
Raghav893 <raghavarora2309@gmail.com>
2026-03-03 19:06:14 +0530

    follower repo

---
+++ src/main/java/com/raghav/xclone/follow/repo/followRepository.java
@@ -0,0 +1,7 @@
+package com.raghav.xclone.follow.repo;
+
+import com.raghav.xclone.follow.entity.follow;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface followRepository extends JpaRepository<follow, Long> {
+}
