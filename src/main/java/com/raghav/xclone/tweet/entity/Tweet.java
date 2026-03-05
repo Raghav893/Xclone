7baf63f9bd0003dd62f79b033af0dddd32e2a73a
Raghav893 <raghavarora2309@gmail.com>
2026-03-05 18:00:19 +0530

    Tweet entity

---
+++ src/main/java/com/raghav/xclone/tweet/entity/Tweet.java
@@ -0,0 +1,47 @@
+package com.raghav.xclone.tweet.entity;
+
+import com.raghav.xclone.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@AllArgsConstructor
+@Builder
+@Data
+@NoArgsConstructor
+public class Tweet {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private UUID tweetId;
+
+    private String content;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "author_id", nullable = false)
+    private User author;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "parent_tweet_id")
+    private Tweet parentTweet;
+
+    @Column(nullable = false)
+    private int replyCount = 0;
+
+    @Column(nullable = false)
+    private int likeCount = 0;
+
+    @Column(nullable = false)
+    private int retweetCount = 0;
+
+    private String mediaUrl;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+}
